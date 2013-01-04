9cf64ecb25b1c583b3be0856b20b6c367722811d
Kirill Krinkin <kirill.krinkin@gmail.com>
2013-01-05 00:41:41 +0400

    GT-797

--- automation/add_deb_build_task.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-dir_automation="$WEBGEO_HOME/automation"
-dir_tasks="${dir_automation}/deb_build_tasks"
-
-echo "$1" > ${dir_tasks}/$1
--- clean_build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-find ./ -name test.suite | xargs rm
-
-dpkg-buildpackage -rfakeroot -j5
-
-cat ./test_summary.log
