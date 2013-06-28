3c4029b097a384c23027e659118f0d23b297e0fa
Mark Zaslavskiy <mark.zaslavskiy@gmail.com>
2013-06-28 14:20:57 +0400

    test for GT-935 was made more general

--- tst/sanity/single-lite.sh
+++ tst/sanity/single-lite.sh
@@ -83,14 +83,20 @@ then
         exit 1
 fi
 
-response_GT_935_test=`curl -d "{\"login\":\"Mark\') OR id=1;--\",\"password\":\"tkkkkest\"}" http://localhost/service/login`;
+response_GT_935_test_1=`curl -d "{\"login\":\"Mark\') OR id=1;--\",\"password\":\"tkkkkest\"}" http://${INSTANCE}/service/login`;
 correct_result_GT_935='10';
-if ! echo $response_GT_935_test | grep -q -s -F "$correct_result_GT_935"  ;
+if ! echo $response_GT_935_test_1 | grep -q -s -F "$correct_result_GT_935"  ;
 then
-        echo "Fail at GT_935 test"
+        echo "Fail at GT_935 test #1"
         exit 1
 fi
 
+response_GT_935_test_2=`curl -d "{\"login\":\"Mark\' OR id=1;--\",\"password\":\"tkkkkest\"}" http://${INSTANCE}/service/login`;
+if ! echo $response_GT_935_test_2 | grep -q -s -F "$correct_result_GT_935"  ;
+then
+        echo "Fail at GT_935 test #2"
+        exit 1
+fi
 
 echo "Success"
 
