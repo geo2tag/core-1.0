f8b72050cfd1326ad3851cda36abef164d414106
Kirill Krinkin <kirill.krinkin@gmail.com>
2013-07-26 21:02:00 +0400

    GT-961 pulse testing debugging

--- tst/unit/unit_tests_xml.sh
+++ tst/unit/unit_tests_xml.sh
@@ -7,7 +7,11 @@ export LD_LIBRARY_PATH=./debian/build/lib/
 
 
 TESTS=`find ./ -name utest.*`
+
+echo "Found `echo ${TESTS} | wc -l` tests"
+
 for test in ${TESTS}; 
 do
+	echo ${test}
 	${test} -xml 
 done;
