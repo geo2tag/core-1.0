9a46a6c35da6e6579273285efde93f2c6a4ace63
Rodion Tatarkin <rodik-dodik.com@mail.ru>
2014-01-27 12:39:44 +0400

    now Tag is BINARY_TAG if field label instead of field description contain #BLOB and Tag continues to have GUID in the field label

--- src/common/src/DataMarks.cpp
+++ src/common/src/DataMarks.cpp
@@ -241,7 +241,7 @@ double Tag::getDistance(double lat1, double lon1, double lat2, double lon2)
 
 TagType Tag::checkBinaryTag()
 {
-    if (getDescription().contains("#BLOB"))
+    if (getLabel().contains("#BLOB"))
         return TAG_BINARY;
     else
         return TAG_COMMON;
