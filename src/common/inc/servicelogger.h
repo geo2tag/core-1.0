489c41b58343f2fe9be356ff4754892251e90e1d
root <root@red-VirtualBox>
2014-11-20 06:11:48 -0500

    GT-1128 Added log filtration.

--- src/common/inc/servicelogger.h
+++ src/common/inc/servicelogger.h
@@ -3,18 +3,80 @@
 
 #include <QObject>
 #include <QDebug>
+#include <QString>
+#include <QSharedPointer>
 
+//Class, which writes messages to log id their writing is enabled in config
 class Log : public QObject
 {
+
     explicit Log();
-
 public:
+    /*All logging facilities and severites. To add new facility, add it to enum,
+     *and assign it a name in Log::init() in servicelogger.cpp
+     *IMPORTANT: Do not assign numbers to enum's values
+     *undefined facility stands for log captured from qWarning(), qDebug(), qCritical(), qFatal()
+     */
+    enum Facility
+    {
+      undefined_fac,
+      test_fac,
+      test2_fac,
+      facility_last_element__
+    };
+    enum Severity
+    {
+      debug_sev,
+      warning_sev,
+      critical_sev,
+      fatal_sev,
+      severity_last_element__
+    };
     static void init();
-
 };
+//Class, used in logging macros to assemble a string from  macros' call and pass it to Log
+class MessageAssembler
+{
+public:
+    Log::Severity msg_sev;
+    Log::Facility msg_fac;
+    QString contents;//string which will be passed to Log
+    QDebug stream;
+    MessageAssembler(Log::Severity _msg_sev,Log::Facility _msg_fac);
+    ~MessageAssembler();
+    //Returns a temporary instance of assembler
+    static QSharedPointer<MessageAssembler> get_instance(Log::Severity _msg_sev,Log::Facility _msg_fac);
+    //Passes contents of reult to Log
+    void operator+=(MessageAssembler& result);
+};
+//TODO: Find a way to put this definition to servicelogger.cpp.
+template<typename msg_type> MessageAssembler& operator<<(MessageAssembler& target,const msg_type msg)
+{
+    target.stream<<msg<<" ";
+    return target;
+}
+namespace NLog
+{
+    extern bool is_init;
+    //Is log with a certain combination of facility/severity enabled
+    extern bool log_enabled[Log::facility_last_element__][Log::severity_last_element__];
+    //Name of facilities and severeties of log, printed in log file. Also, these names without spaces are used in config file
+    extern QString severity_name[Log::severity_last_element__];
+    extern QString facility_name[Log::facility_last_element__];
 
-#define DEBUG() ( qDebug() << __FILE__<<":"<<__LINE__ )
-#define WARNING() ( qWarning() << __FILE__<<":"<<__LINE__ )
-#define CRITICAL() ( qCritical() << __FILE__<<":"<<__LINE__ )
+    extern MessageAssembler assembler;//Can be replaced with MessageAssembler::get_instance(...,...)
+}
 
+//TODO:  add facilities to calls of logging macros and delete __ after names of the following macros. Delete logging macros with no arguments
+#define DEBUG__(facility) NLog::assembler+=(*MessageAssembler::get_instance(Log::debug_sev,facility))<< __FILE__<<":"<<__LINE__
+#define WARNING__(facility) NLog::assembler+=(*MessageAssembler::get_instance(Log::warning_sev,facility))<< __FILE__<<":"<<__LINE__
+#define CRITICAL__(facility) NLog::assembler+=(*MessageAssembler::get_instance(Log::critical_sev,facility))<< __FILE__<<":"<<__LINE__
+//Note: FATAL() macro will terminate the application
+#define FATAL__(facility) NLog::assembler+=(*MessageAssembler::get_instance(Log::fatal_sev,facility))<< __FILE__<<":"<<__LINE__
+
+
+#define DEBUG() DEBUG__(Log::test_fac)
+#define WARNING() WARNING__(Log::test_fac)
+#define CRITICAL() CRITICAL__(Log::test_fac)
+#define FATAL() FATAL__(Log::test_fac)
 #endif // SERVICELOGGER_H
--- src/common/src/servicelogger.cpp
+++ src/common/src/servicelogger.cpp
@@ -1,48 +1,139 @@
 #include "servicelogger.h"
 #include <QFile>
-#include <QTextStream>
 #include <QDateTime>
 #include <QDebug>
+#include <QString>
+#include <QStringList>
+#include <QSharedPointer>
+#include "SettingsStorage.h"
 
 #include <syslog.h>
 
-static const char *file="/var/log/geo2tag.log";
 
+static const char *file="/var/log/geo2tag.log";
+QString NLog::severity_name[Log::severity_last_element__];
+QString NLog::facility_name[Log::facility_last_element__];
+bool NLog::log_enabled[Log::facility_last_element__][Log::severity_last_element__];
+bool NLog::is_init=false;
+using namespace NLog;
+static void handler(QtMsgType type, const char *msg);
+
+void write_to_log(const char *msg, Log::Severity sev, Log::Facility fac)
+{
+    if(log_enabled[fac][sev])
+    {
+        static QFile output(file);
+
+        if(!output.isOpen())
+        {
+            if(!output.open(QIODevice::ReadWrite|QIODevice::Append))
+            {
+                //this is only place for syslog
+                syslog(LOG_ERR,"Geo2tag service ERROR, cannot open log file %s",file);
+            }
+            QTextStream beginlog(&output);
+            beginlog << "=== BEGIN LOG at " << QDateTime::currentDateTimeUtc().toString(Qt::ISODate) << "===" << endl;
+        }
+        QTextStream stream(&output);
+        stream << QDateTime::currentDateTimeUtc().toString(Qt::ISODate);
+        stream<<" ["<<facility_name[fac]<<" : "<<severity_name[sev]<<"] ";
+        stream<<msg<<endl;
+    }
+    //Terminate the application if FATAL() macro was called
+    if(fac!=Log::undefined_fac&&sev==Log::fatal_sev)
+    {
+        qInstallMsgHandler(0);
+        qFatal(" ");
+    }
+}
+MessageAssembler:: MessageAssembler(Log::Severity _msg_sev,Log::Facility _msg_fac):msg_sev(_msg_sev),msg_fac(_msg_fac),stream(&contents)
+{
+}
+MessageAssembler::~MessageAssembler()
+{
+}
+//yasper::ptr<MessageAssembler> MessageAssembler::get_instance(Log::Severity _msg_sev,Log::Facility _msg_fac)
+QSharedPointer<MessageAssembler> MessageAssembler::get_instance(Log::Severity _msg_sev,Log::Facility _msg_fac)
+{
+    QSharedPointer<MessageAssembler> res=QSharedPointer<MessageAssembler> (new MessageAssembler(_msg_sev,_msg_fac));
+    return res;
+}
+MessageAssembler NLog::assembler(Log::severity_last_element__,Log::facility_last_element__);
+void MessageAssembler::operator+=(MessageAssembler& result)
+{
+    write_to_log(result.contents.toStdString().c_str(),result.msg_sev,result.msg_fac);
+}
+
+//Captures messages from qWarning(), qDebug(), qCritical(), qFatal()
 static void handler(QtMsgType type, const char *msg)
 {
-    static QFile output(file);
-
-    if(!output.isOpen())
-    {
-        if(!output.open(QIODevice::ReadWrite|QIODevice::Append))
-        {
-            //this is only place for syslog
-            syslog(LOG_ERR,"Geo2tag service ERROR, cannot open log file %s",file);
-        }
-        QTextStream beginlog(&output);
-        beginlog << "=== BEGIN LOG at " << QDateTime::currentDateTimeUtc().toString(Qt::ISODate) << "===" << endl;
-    }
-
-    QTextStream stream(&output);
-    stream << QDateTime::currentDateTimeUtc().toString(Qt::ISODate);
-
+    Log::Severity sev;
     switch (type)
     {
     case QtDebugMsg:
-        stream << " [DBG]: " << msg << endl;
+        sev=Log::debug_sev;
         break;
     case QtWarningMsg:
-        stream << " [WRN]: " << msg << endl;
+        sev=Log::warning_sev;
         break;
     case QtCriticalMsg:
-        stream << " [CRT]: " << msg << endl;
+        sev=Log::critical_sev;
         break;
     case QtFatalMsg:
-        stream << " [FTL]: " << msg << endl;
+        sev=Log::fatal_sev;
     }
+    write_to_log(msg,sev,Log::undefined_fac);
 }
-
 void Log::init()
 {
+    if(is_init)
+        return;
+    severity_name[Log::debug_sev]="debug";
+    severity_name[Log::warning_sev]="warning";
+    severity_name[Log::critical_sev]="critical";
+    severity_name[Log::fatal_sev]="fatal";
+
+    facility_name[Log::undefined_fac]="undefined";
+    facility_name[Log::test_fac]="test";
+    facility_name[Log::test2_fac]="Flying Spagetti Monster";
+
+    /*By default all log messages are enabled. Severity mode x specified
+     *for facility y, means that log with facility y and severity y is
+     *enabled. "none" specified for a facility will disable all messages from it
+     */
+    QString default_log_mode("");
+    for(int i=0;i<Log::severity_last_element__;++i)
+    {
+        default_log_mode+=severity_name[i];
+        if(i+1!=Log::severity_last_element__)
+            default_log_mode+=",";
+    }
+    for(int i=0;i<Log::facility_last_element__;++i)
+    {
+        QString formatted_facility_name=facility_name[i].simplified();
+        formatted_facility_name.replace(" ","");
+        QVariant saved_data=SettingsStorage::getValue("Log_Settings/"+formatted_facility_name,QVariant(default_log_mode.split(",")));
+
+        QList<QVariant> list=saved_data.toList();
+        //QVariant doesn't convert a single string to list
+        if(list.empty())
+            list.append(saved_data);
+
+        for(QList<QVariant>::iterator j=list.begin();j!=list.end();++j)
+        {
+            QString st=j->toString();
+            if(st=="none")
+                break;
+            for(int k=0;k<Log::severity_last_element__;++k)
+            {
+                if(severity_name[k]==st)
+                {
+                    log_enabled[i][k]=true;
+                    break;
+                }
+            }
+        }
+    }
     qInstallMsgHandler(handler);
+    is_init=true;
 }
